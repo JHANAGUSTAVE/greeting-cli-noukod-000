bc98e9a383c4cda1151a170dac568fdb676961e5
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-04 18:15:11 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,8 +1,5 @@
 # code the #greeting method here!
 def greeting(name)
-  name = "Don"
-  name = name.strip
   puts ("Hello #{name}. It's nice to meet you.")
-  puts("Hello Sally. It's nice to meet you.")
 end
 
